8c6bc57db858c175a060b16b8f716a8c2175d72a
smarten <smarten.developer@gmail.com>
2020-08-25 19:32:21 -0700

    more work

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,15 @@
-$(document).ready(function() {
-    
-})
+$(document).ready(function () {
+  // Declar constants
+  const $search = $(".search");
+  const $input = $(".input");
+  const apiKey = "d591982fee792f7bb7e113410b2ec08f";
+
+  $search.on("click", function () {
+    let searchValue = $input.val();
+    $.get(
+      `api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=${apiKey}`
+    ).then(function (resp) {
+      console.log(resp);
+    });
+  });
+});
